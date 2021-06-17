5412afe520b5e847b76182c41ba6397d7e24322c
Jador de Vries <jador.de.vries@student.nhlstenden.com>
2021-06-17 11:57:39 +0200

    Composite bugfixes

--- DesignPatterns/Commands/MergeToGroup.cs
+++ DesignPatterns/Commands/MergeToGroup.cs
@@ -12,6 +12,7 @@ namespace DesignPatterns.Commands
     class MergeToGroup : ICommand
     {
         private readonly List<BaseControl> _selection;
+        private CompositeControl _group;
         public MergeToGroup(List<BaseControl> selection)
         {
             _selection = selection;
@@ -33,15 +34,19 @@ namespace DesignPatterns.Commands
             int w = (int)(lastX.X - x + lastX.Width);
             int h = (int)(lastY.Y - y + lastY.Height);
 
-            CompositeControl group = MainWindow.mainWindow.canvas.CreateShape(ShapeType.Group, new System.Windows.Point(x, y), w, h) as CompositeControl;
+            CompositeControl _group = MainWindow.mainWindow.canvas.CreateShape(ShapeType.Group, new System.Windows.Point(x, y), w, h) as CompositeControl;
 
-            MainWindow.mainWindow.canvas.Children.Add(group);
-            MainWindow.mainWindow.canvas.Group.Add(group);
+            MainWindow.mainWindow.canvas.Children.Add(_group);
         }
 
         public void Reverse()
         {
-            // Tijdsgebrek
+            MainWindow.mainWindow.canvas.Children.Remove(_group);
+            MainWindow.mainWindow.canvas.Group.Remove(_group);
+            foreach (BaseControl item in _selection)
+            {
+                MainWindow.mainWindow.canvas.Group.Add(item);
+            }
         }
     }
 }
